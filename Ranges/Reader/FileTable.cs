ca200efaebeab543d64746591586a44b05d9b087
Sergio Magaña <sergiotem@gmail.com>
2016-08-19 20:28:01 +0200

    Renamed files

---
+++ Ranges/Reader/FileTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranges.Reader
+{
+    public class FileTable
+    {
+        public List<FileTableCell> Cells;
+
+        public FileTable()
+        {
+            Cells = new List<FileTableCell>();
+        }
+    }
+}

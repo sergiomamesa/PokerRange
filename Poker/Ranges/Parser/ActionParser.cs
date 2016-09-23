acd08615829c87b29d3a7c00f24efcdb11258669
Sergio Magaña <sergiotem@gmail.com>
2016-09-23 08:19:31 +0200

    Add dictionary to ActionParse reduce complexity

--- Poker/Ranges/Parser/ActionParser.cs
+++ Poker/Ranges/Parser/ActionParser.cs
@@ -9,31 +9,25 @@ namespace Poker.Ranges.Parser
 {
     public class ActionParser
     {
+        private Dictionary<string, ActionType> Map;
+
+        public ActionParser()
+        {
+            Map = new Dictionary<string, ActionType>();
+            Map.Add("F", ActionType.Fold);
+            Map.Add("L", ActionType.Limp);
+            Map.Add("C", ActionType.Call);
+            Map.Add("R", ActionType.Raise);
+            Map.Add("RF", ActionType.RaiseOrFold);
+            Map.Add("RC", ActionType.RaiseOrCall);
+            Map.Add("RCF", ActionType.RaiseOrCallOrFold);
+            Map.Add("CF", ActionType.CallOrFold);
+        }
+
         public ActionType Parse(string action)
         {
-            if (action == "F")
-                return ActionType.Fold;
-
-            if (action == "L")
-                return ActionType.Limp;
-
-            if (action == "C")
-                return ActionType.Call;
-
-            if (action == "R")
-                return ActionType.Raise;
-
-            if (action == "RF")
-                return ActionType.RaiseOrFold;
-
-            if (action == "RC")
-                return ActionType.RaiseOrCall;
-
-            if (action == "RCF")
-                return ActionType.RaiseOrCallOrFold;
-
-            if (action == "CF")
-                return ActionType.CallOrFold;
+            if (Map.ContainsKey(action))
+                return Map[action];
 
             throw new NotImplementedException();
         }
